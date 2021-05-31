22487a14516e02c48acb97ec7cb285e4ca684ded
mhbahmani <mhbahmani79@gmail.com>
2021-05-31 17:18:03 +0430

    add reformat_username func

--- utils.py
+++ utils.py
@@ -8,3 +8,7 @@ def event_data_to_str(event_data: Dict[str, str]) -> str:
 
     facts = [f'{key}: {value}' for key, value in event_data.items()]
     return "\n".join(facts).join(['\n', '\n'])
+
+
+def reformat_username(username: str) -> str:
+    return '@{}'.format(username.replace("_", "\\_"))
