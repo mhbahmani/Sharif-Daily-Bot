7052ca8bca0a1e5bfc584d130cc27f2ad61b3e03
mhbahmani <mhbahmani79@gmail.com>
2021-05-31 11:27:12 +0430

    set default value to env vars

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from collections import defaultdict
 from decouple import config
 from app import SharifDailyBot
 
 
 SharifDailyBot(
     config('TOKEN'), 
-    config('ADMIN_ID'),
-    config('LOG_LEVEL')).run()
+    config('ADMIN_ID', cast=int),
+    config('LOG_LEVEL', default='INFO')
+    ).run()
